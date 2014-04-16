9a4578507276e8c696a3023e8e28a9b41af456ef
Alex Smith <alexjs@swiftserve.com>
2014-04-16 12:00:56 +0800

    Python source cleanup

--- security/token/python/token_tools.py
+++ security/token/python/token_tools.py
@@ -1,8 +1,31 @@
+"""
+
+This construction allows you to demo code using a preset example
+you can use this code directly with:
+
+    from token_generator import sign_url
+
+For general use,
+Example URL:
+/path/to/protect?stime=20081201060100&etime=20151201060100
+
+Where stime= is the start time, and etime is the end time
+(both following yyyymmddHHMMSS format)
+
+Please *note*: for SS authentication, the `stime' and `etime'
+arguments in the query string are mandatory as part of the
+resource specifier.
+
+Tokens optionally support the use of 'ip' parameter, e.g.:
+/path/to/protect?stime=20081201060100&etime=20151201060100&ip=10.0.0.5
+
+"""
+
 import hashlib
 import hmac
 import argparse
 
-parser = argparse.ArgumentParser(prog="token_tools.py",
+parser = argparse.ArgumentParser(prog="token_generator.py",
     description='''A sample token authentication app for Python''')
 parser.add_argument('-u', '--url', nargs=1,
     help='URL to parse (path only)', required=True)
@@ -13,21 +36,20 @@ params = parser.parse_args()
 __all__ = ["generate_token", "sign_url"]
 
 
-def generate_token(resource,secret_key):
+def generate_token(resource, secret_key):
+    """ Generate the token based on the resource (URL) and the secret key
+    taken from the portal. This is based on the first 20 characters of the
+    sha1sum of the secret key and the URL
+    """
     hmac_builder = hmac.new(secret_key, resource, hashlib.sha1)
     return "0%s" % (hmac_builder.hexdigest()[:20])
 
-def sign_url(url,secret):
-    token=generate_token(url,secret)
+def sign_url(url, secret):
+    """ Return the signed URL with the token added as a new queryString argument
+    ("encoded").
+    """
+    token = generate_token(url, secret)
     return url + "&encoded=" + token
-# Example URL:
-# /path/to/protect?stime=20081201060100&etime=20151201060100
-# Where stime= is the start time, and etime is the end time
-# both following the format yyyymmddHHMMSS 
-# 
-# Tokens optionally support the use of 'ip'
 
-#url = sign_url('insert url here','insert key from portal here')
-
-url = sign_url(params.url[0], params.key[0])
-print url
+signedUrl = sign_url(params.url[0], params.key[0])
+print signedUrl
