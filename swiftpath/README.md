1e85e29f8a494e597f8232a8e979433dacd3816e
Alex Smith <alexjs@swiftserve.com>
2014-05-29 18:32:01 +0800

    Adding SwiftPath info

---
+++ swiftpath/README.md
@@ -0,0 +1,8 @@
+SwiftPath User Insight Tag
+=======================
+
+Place this tag in the HTML of the pages you wish to measure. We recommend placing it just before the closing </body> tag
+
+It will automatically send information to a secure data cloud which can then be used for better route optimisation
+
+For further information, see the SwiftServe manual or raise an issue/ticket.
---
+++ swiftpath/insight.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+  (function(w, d) { var a = function() { var b = d.createElement('script'); b.type = 'text/javascript';
+        if (undefined !== b.setAttribute) { b.setAttribute('async', 'async'); }
+          b.src = '//' + ((w.location.protocol === 'https:') ? 's3.amazonaws.com/cdx-radar/' :
+          'radar.cedexis.com/') + '01-11851-radar10.min.js'; d.body.appendChild(b); };
+    if (w.addEventListener) { w.addEventListener('load', a, false); }
+      else if (w.attachEvent) { w.attachEvent('onload', a); }
+        }(window, document));
+</script>
